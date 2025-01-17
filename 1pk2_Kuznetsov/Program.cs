22f34e79e13942e083217136ef8bb90981d2160a
Рыбак <@DESKTOP-5AJ2K2S>
2025-01-17 21:27:35 +0500

    Добавьте файлы проекта.

---
+++ 1pk2_Kuznetsov/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1pk2_Kuznetsov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
