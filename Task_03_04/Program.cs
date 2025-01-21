0840673e816336e11503d93e8fc2f27c0cf0d428
Владислав Кузнецов <vladkrus2.9@gmail.com>
2025-01-21 17:47:15 +0500

    переименован проект

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Пользователь вводить в консоли произвольный текст. После каждого ввода консоль очищается.Когда
+            пользователь вводить слово «exit» или пустую строку – ввод останавливается и выводиться количество строк
+            введенных пользователем.*/
+
+            while (true)
+            {
+
+                Console.Write("Введите текст: ");
+                string text = Console.ReadLine();
+                Console.Clear();
+
+                int quantity = 0;
+
+                while (text != "exit" || text != null)
+                {
+                    quantity++;
+                    Console.Write("Введите текст: ");
+                    text = Console.ReadLine();
+                    Console.Clear();
+                }
+                Console.WriteLine(quantity);
+                break;
+            }
+        }   
+    }
+}
