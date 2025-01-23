8cdbbc0b875d5c18ef503c96ede061360399f9c5
Владислав Кузнецов <vladkrus2.9@gmail.com>
2025-01-23 17:00:46 +0500

    Oтредактированны 5 задача урока 3. И сделаны задачи 6 и 7 урока 3.

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_03_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Написать программу, которая выводит на экран таблицу соответствия температуры в градусах Цельсия и
+            Фаренгейта(F = C * 1,8 + 32).Диапазон изменения температуры в градусах Цельсия и шаг должны вводиться во
+            время работы программы*/
+
+            Console.Write("Введите диапазон: \nОт: ");
+            double range_1 = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("До: ");
+            double range_2 = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Введите шаг: ");
+            double step = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+            
+            Console.WriteLine("Цельсия - Фаренгейт");
+            while (range_1 <= range_2)
+            {
+                double faren = range_1 * 1.8 + 32;
+                Console.WriteLine("  " + range_1 + "\t-    " + faren);
+                range_1 += step;
+            }
+            
+            
+        }
+    }
+}
---
+++ Task_03_07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Task_03_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Написать программу, которая выводит таблицу скорости(через каждые 0, 5с) свободно падающего тела v = g t ,
+            где 2 g = 9,8 м / с2 – ускорение свободного падения.*/
+
+            double g = 9.8;
+            double t = 0.5;
+
+            Console.WriteLine("Скорость свободно падающего тела: \nВремя: \t   Скорость: ");
+
+            Console.Write("Введите лимит: ");
+            double limit = Int32.Parse(Console.ReadLine());
+
+            while (t <= limit)
+            {
+                double v = g * t;
+                Console.WriteLine(t + "     -     " + v);
+                t += 0.5;
+            }
+        }
+    }
+}
