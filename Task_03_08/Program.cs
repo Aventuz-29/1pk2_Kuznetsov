9a6184ddf10ecc388aa8d79824dbd0ca8ccce1bc
Владислав Кузнецов <vladkrus2.9@gmail.com>
2025-01-23 17:22:42 +0500

    Решена задача номер 8 урока 3.

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны натуральные числа от 20 до 50.Напечатать те из них, которые делятся на 3, но не делятся на 5.
+
+            int num = 20;
+
+            Console.Write("Числа которые делятся на 3, но не делятся на 5: ");
+
+            for (int i = num; i <= 50; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                    Console.Write(i + ", ");
+            }
+        }
+    }
+}
