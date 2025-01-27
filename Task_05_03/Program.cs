873418a8c7839c3036035f74a57efb63b1bfd0a3
Владислав Кузнецов <vladkrus2.9@gmail.com>
2025-01-27 21:25:37 +0500

    Задача 3 урока 5 полностью решена

--- Task_05_03/Program.cs
+++ Task_05_03/Program.cs
@@ -12,8 +12,9 @@
             char[,] two = new char[3, 3];
             Random rnd = new Random();
             int num = 0;
-            
-            //Если надо проверить на то что матрицы равны или нет:
+
+            /*Если надо проверить на то что матрицы равны или нет(если вы проверяете через ручное задания массива и
+            комментируете часть кода где создаются массивы, то изначальные массивы выводится не будут!!!*/
 
             /*char[,] one = { { 'a', 's', 'd'},{ 'a', 's', 'd'},
             { 'a', 's', 'd'} };
@@ -21,29 +22,30 @@
             { 'a', 's', 'd'} };*/
 
             //Создание двух массивов
-            for(int i  = 0; i < one.GetLength(0); i++)
+            for (int i = 0; i < one.GetLength(0); i++)
             {
                 for (int j = 0; j < one.GetLength(1); j++)
                 {
                     one[i, j] = (char)rnd.Next('a', 'z' + 1);
-                    
-                    Console.Write(one[i, j]);
+
+                    Console.Write(one[i, j] + " ");
                 }
-            }
-            Console.WriteLine();
-            for (int i = 0; i < two.GetLength(0); i++)
-            {
-                for (int j = 0; j < two.GetLength(1); j++)
+
+                Console.Write("   ");
+
+                for (int p = 0; p < two.GetLength(1); p++)
                 {
-                    two[i, j] = (char)rnd.Next('a', 'z' + 1);
-                    Console.Write(two[i, j]);
+                    two[i, p] = (char)rnd.Next('a', 'z' + 1);
+                    Console.Write(two[i, p] + " ");
                 }
+
+                Console.WriteLine();
             }
 
             //Проверка на то равны ли матрицы
-            for(int i = 0; i < one.GetLength(0); i++)
+            for (int i = 0; i < one.GetLength(0); i++)
             {
-                for(int j = 0; j < one.GetLength(1); j++)
+                for (int j = 0; j < one.GetLength(1); j++)
                 {
 
                     if (one[i, j] == two[i, j])
@@ -62,24 +64,39 @@
             //Матрицы с цветовой индикацией
             if (num != one.GetLength(0) * one.GetLength(1))
             {
-                Console.WriteLine();
+                Console.WriteLine("=>");
                 for (int i = 0; i < one.GetLength(0); i++)
                 {
                     for (int j = 0; j < one.GetLength(1); j++)
                     {
-
                         if (one[i, j] == two[i, j])
                         {
-                            num++;
                             Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Write(one[i, j]);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Write(" ");
                         }
-                        Console.Write(one[i, j] + " " + two[i, j]);
-                        Console.BackgroundColor = ConsoleColor.Black;
-                        Console.WriteLine();
+                        else Console.Write(one[i, j] + " ");
                     }
-                }
-            }
 
+                    Console.Write("   ");
+
+                    for (int p = 0; p < two.GetLength(1); p++)
+                    {
+                        if (one[i, p] == two[i, p])
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Write(two[i, p]);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Write(" ");
+                        }
+                        else Console.Write(two[i, p] + " ");
+                    }
+
+                    Console.WriteLine();
+                }
+
+            }
         }
     }
 }
