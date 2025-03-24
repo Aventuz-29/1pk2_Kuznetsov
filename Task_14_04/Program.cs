c60651226fa413b86cf02a04d15871a941449ed3
Владислав Кузнецов <vladkrus2.9@gmail.com>
2025-03-24 21:37:37 +0500

    Задача 4 урока 14 решена

---
+++ Task_14_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using Task_14_04;
+
+namespace Task_14_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определите класс User, который будет иметь статическое свойство CurrentUser,
+            //представляющеетекущегопользователя, и метод для его установки.
+
+            // Создаем пользователя
+            User user1 = new User("Влад", 1);
+
+            // Устанавливаем его как текущего
+            User.SetCurrentUser(user1);
+
+            // Получаем текущего пользователя
+            User current = User.CurrentUser;
+            Console.WriteLine($"Текущий юзер: {current.Name}, ID: {current.Id}");
+        }
+    }
+}
---
+++ Task_14_04/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_04
+{
+    class User
+    {
+        // статическое свойство, представляющее текущего пользователя
+        public static User CurrentUser { get; set; }
+
+        /// <summary>
+        /// метод для установки текущего пользователя
+        /// </summary>
+        /// <param name="user">данные о пользователе</param>
+        public static void SetCurrentUser(User user)
+        {
+            CurrentUser = user;
+        }
+
+        // cвойства пользователя
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="name">имя</param>
+        /// <param name="id">айди</param>
+        public User(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
